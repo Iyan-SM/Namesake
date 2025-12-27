02e7d0ab4a00c1fad4ea53d17e32a6e2b6322cb1
Iyan SM <iyansyeedmiller@gmail.com>
2025-12-26 20:31:02 -0700

    Revert "fixed filenaming and proper download of resume"
    This reverts commit c0afe3588d7ba9afde62b53b73fd13f09de92e6f.

---
+++ namesake/src/routes/+page.svelte
@@ -0,0 +1,234 @@
+<script>
+	function openInNewTab(url) {
+		window.open(url, '_blank').focus();
+	}
+	let width = $state(200);
+
+	function downloadResume(){
+		const link = document.createElement('a');
+		link.style.display = 'none';
+		link.href = '/resume.png';
+		link.download = 'Resume_Iyan_Syeed-Miller';
+
+		// It needs to be added to the DOM so it can be clicked
+		document.body.appendChild(link);
+		link.click();
+
+		// To make this work on Firefox we need to wait
+		// a little while before removing it.
+		setTimeout(() => {
+			URL.revokeObjectURL(link.href);
+			link.parentNode.removeChild(link);
+		}, 0);
+	}
+</script>
+<svelte:window bind:innerWidth={width}/>
+<div class='page'>
+	<br>
+	<br>
+	<header>
+		<h1>Iyan</h1>
+		<h1>Syeed-Miller</h1>
+	</header>
+	<br>
+	<div class="links">
+		<button onclick={openInNewTab("https://github.com/Fireg53")} class="bioLink"><i class="fab fa-github"></i><span>Fireg53</span></button>
+		<button onclick={openInNewTab("https://www.youtube.com/@fireg5355")} class="bioLink"><i class="fab fa-youtube"></i> <span>Fireg5355</span></button>
+		<button onclick={openInNewTab("https://www.instagram.com/iyan.in3d/")} class="bioLink"><i class="fab fa-instagram"></i>Iyan.in3d</button>
+		<button onclick={openInNewTab("mailto:iyansyeedmiller@gmail.com")} class="bioLink"><i class="fas fa-envelope"></i> <span>Iyansyeedmiller@gmail.com</span></button>
+	</div>
+	<div class="bodyContainer">
+		<div class="resumeBorder gradient">
+			<span class='label'><p>Resume</p><button class='download' onclick={downloadResume()}><i class="fas fa-download"></i></button></span>
+			<img  class="resume" src="resume.png" alt='Resume' style='width:100%; height:100%; border-radius:6px;'>
+		</div>
+		<!--
+		<div class="thumbnailContainer">
+			<div class="thumbnail gradient"></div>
+			<div class="thumbnail gradient"></div>
+			<div class="thumbnail gradient"></div>
+		</div>
+		-->
+	</div>
+</div>
+
+<style>
+	.page{
+		padding: 20px;
+	}
+	h1{
+		font-family: "Climate Crisis", sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 400;
+		font-style: normal;
+		font-variation-settings:
+			"YEAR" 1979;
+		color: #29274C;
+		font-size: 6.5vw;
+		line-height: 2rem;
+		margin-left: 5%;
+		margin-right: 10%;
+		margin-top: 4rem;
+	}
+	.links{
+		display: flex;
+		column-gap: 2rem;
+		color: #29274C;
+		margin-left: 5%;
+	}
+
+	@media (max-width: 800px) {
+		.bodyContainer{
+			flex-direction: column;
+		}
+		.thumbnailContainer{
+			flex-direction: row;
+		}
+	}
+	@media (min-width: 801px) {
+		.thumbnailContainer{
+			flex-direction: column;
+		}
+	}
+
+
+	.bodyContainer{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.thumbnailContainer {
+		display: flex;
+		justify-content: space-between;
+		margin-left: 8vw;
+		margin-right: 8vw;
+		margin-top: 5rem;
+		height: 70vw;
+	}
+	
+	.thumbnail {
+		
+		width: 20vw;
+		height: 20vw;
+	}
+
+	.resumeBorder{
+		display: flex;
+		flex-direction: column;
+		padding: 20px;
+		margin-left: 5vw;
+		margin-top: 5rem;
+		margin-right: 5vw;
+		border-radius: 20px;
+	}
+
+	.resume{
+		width: 55vw;
+		height: 70vw;
+	}
+
+	.gradient{
+		background-image: linear-gradient( #E6BCCD, #D295BF, #7E52A0);
+	}
+
+	.label{
+		padding-left: 2vw;
+		padding-right: 2vw;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		color: white;
+		font-family: "Fugaz One", sans-serif;
+		font-weight: 400;
+		font-style: normal;
+		font-size: 1.5vw;
+		line-height: 0.2vw;
+
+	}
+	.download {
+		background-color:#E6BCCD;
+		border: 0;
+		color: white;
+		cursor: pointer;
+	}
+	.bioLink {
+	align-items: center;
+	background-image: linear-gradient(144deg,#E6BCCD, #D295BF 20%, #7E52A0);
+	border: 0;
+	border-radius: 8px;
+	box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+	box-sizing: border-box;
+	color: #FFFFFF;
+	display: flex;
+	font-family: Phantomsans, sans-serif;
+	font-size: 1em;
+	justify-content: center;
+	column-gap: 0.5em;
+	max-width: 30%;
+	min-width: 2em;
+	padding: 8px 12px; 
+	text-decoration: none;
+	user-select: none;
+	touch-action: manipulation;
+	white-space: nowrap;
+	cursor: pointer;
+	transition: box-shadow ease 0.5s;
+	}
+
+	.bioLink:active,
+	.bioLink:not([disabled]):hover {
+		box-shadow:  -.125rem -.125rem 0.5rem #E6BCCD, .125rem .125rem 0.5rem #c085ee;
+	}
+
+	/*Page Transitions*/
+	:root {
+	--paperAngle: 20deg;
+	}
+
+	@keyframes fade-in {
+		from {
+			opacity: 0;
+		}
+	}
+
+	@keyframes fade-out {
+		to {
+			opacity: 0;
+		}
+	}
+
+	@keyframes slide-from-top {
+		from {
+			transform: translateY(-100vh);
+		}
+	}
+
+	@keyframes slide-from-bottom {
+		from {
+			transform: translateY(100vh);
+		}
+	}
+
+	@keyframes spin {
+	from {
+		transform: rotate(20deg);
+	}
+	}
+
+
+	:root::view-transition-old(root) {
+	z-index: 1; /* Behind the new page */
+	animation: 3000ms;
+	}
+
+	:root::view-transition-new(root) {
+	z-index: 2;
+		animation:
+		1000ms cubic-bezier(0.4, 0, 0.2, 1) spin,
+		210ms cubic-bezier(0, 0, 0.2, 1) 90ms fade-in,
+		1000ms cubic-bezier(0.4, 0, 0.2, 1) slide-from-bottom;
+	}
+
+</style>
